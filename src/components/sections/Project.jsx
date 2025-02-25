d8eaed63639d71b1b92be81a7dea4670266654f6
Lucie Gernidos <lgernido@bess-f4r1s7.clusters.42paris.fr>
2025-02-25 16:00:53 +0100

    all projects completed

--- src/components/sections/Project.jsx
+++ src/components/sections/Project.jsx
@@ -100,7 +100,7 @@ export const Projects = () => {
                         ))}
                         </div>
                         <div className="flex justify-between items-center">
-                            <a href="#minishell" className="text-blue-400 hover:text-blue-300 transition-colors my-4">
+                            <a href="https://github.com/lgernido/minishell" className="text-blue-400 hover:text-blue-300 transition-colors my-4">
                                 View Project → 
                             </a>
                         </div>
@@ -127,10 +127,10 @@ export const Projects = () => {
                     <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.1)] transition-all">
                         <h3 className="text-xl font-bold mb-2">Cub3d</h3>
                         <p className="text-gray-400 mb-4">
-                            Learn the basics of threading a process to solve the philosphers problem
+                            Use raycasting and minilibx to render a game based on Wolfenstein 3D 
                         </p>
                         <div className="flex flex-wrap gap-2 mb-4">
-                            {["C", "Threads", "Mutexes"].map((tech, key) => (
+                            {["C", "Raycasting", "3D", "Minilibx", "Gaming"].map((tech, key) => (
                             <span key={key}
                             className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.2)] transition">
                                 {tech}
@@ -138,7 +138,7 @@ export const Projects = () => {
                         ))}
                         </div>
                         <div className="flex justify-between items-center">
-                            <a href="https://github.com/lgernido/42-philo" target="blank" rel="noopener noreferrer" className="text-blue-400 hover:text-blue-300 transition-colors my-4">
+                            <a href="https://github.com/lgernido/cub3d" target="blank" rel="noopener noreferrer" className="text-blue-400 hover:text-blue-300 transition-colors my-4">
                                 View Project → 
                             </a>
                         </div>
